9a1418fa7e0a0b0f6e685bf124d490ffa2ee440e
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-12 20:46:41 +0900

    create AceClient Plugin

---
+++ app/Plugin/AceClient/Entity/Config.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Plugin\AceClient\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+if (!class_exists('\Plugin\AceClient\Entity\Config', false)) {
+    /**
+     * Config
+     *
+     * @ORM\Table(name="plg_ace_client_config")
+     * @ORM\Entity(repositoryClass="Plugin\AceClient\Repository\ConfigRepository")
+     */
+    class Config
+    {
+        /**
+         * @var int
+         *
+         * @ORM\Column(name="id", type="integer", options={"unsigned":true})
+         * @ORM\Id
+         * @ORM\GeneratedValue(strategy="IDENTITY")
+         */
+        private $id;
+
+        /**
+         * @var string
+         *
+         * @ORM\Column(name="name", type="string", length=255)
+         */
+        private $name;
+
+        /**
+         * @return int
+         */
+        public function getId()
+        {
+            return $this->id;
+        }
+
+        /**
+         * @return string
+         */
+        public function getName()
+        {
+            return $this->name;
+        }
+
+        /**
+         * @param string $name
+         *
+         * @return $this;
+         */
+        public function setName($name)
+        {
+            $this->name = $name;
+
+            return $this;
+        }
+    }
+}
