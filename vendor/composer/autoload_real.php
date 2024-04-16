a44bb3235f7a8e4ad907bc83a5e2547279d15909
Nguyen Van Thong <v.t.nguyen@ar-system.co.jp>
2024-04-16 20:58:21 +0900

    delete var/

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInitb05fb2f10279b2e65fbb66f5e802c8cb
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitb05fb2f10279b2e65fbb66f5e802c8cb::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+        $requireFile = static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        }, null, null);
+        };
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
+            ($requireFile)($fileIdentifier, $file);
         }
 
         return $loader;
